3bbc3573079b6bef0c25e35d4e79e543aef71b52
emted16 <emted16@gmail.com>
2025-03-08 00:37:20 +0200

    QuestionsScreen Center

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -3,22 +3,22 @@ import 'package:flutter/material.dart';
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
 
-  
   @override
   State<QuestionsScreen> createState() {
     return _QuestionsScreenState();
-    
   }
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
-
   @override
   Widget build(context) {
-    return const Text('QuestionsScreen');
+    return const Center(
+      child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        Text(
+          'Quiz is Starting MadaFaka',
+          style: TextStyle(color: Colors.white, fontSize: 24),
+        )
+      ]),
+    );
   }
 }
-
-
-
-
