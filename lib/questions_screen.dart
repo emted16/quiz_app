23e8be33aeaa751f536baee3392e1e2b8cc17f85
emted16 <emted16@gmail.com>
2025-03-15 00:03:58 +0200

    btn

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/answer_button.dart';
+import 'package:quiz_app/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -12,32 +14,28 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
+    final currentQuestion = questions[0];
+
     return Center(
-      child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-        const Text(
-          'Questions List...',
-          style: TextStyle(color: Colors.white, fontSize: 24),
-        ),
-        const SizedBox(
-          height: 30,
-        ),
-        ElevatedButton(
-          onPressed: () {},
-          child: const Text('Answer 1'),
-        ),
-        ElevatedButton(
-          onPressed: () {},
-          child: const Text('Answer 2'),
-        ),
-        ElevatedButton(
-          onPressed: () {},
-          child: const Text('Answer 3'),
-        ),
-        ElevatedButton(
-          onPressed: () {},
-          child: const Text('Answer 4'),
-        ),
-      ]),
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                currentQuestion.text,
+                style: const TextStyle(color: Colors.white, fontSize: 14),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              ...currentQuestion.answers.map((answer) {
+                return AnswerButton(answerText: answer, onTap: () {});
+              }),
+            ]),
+      ),
     );
   }
 }
